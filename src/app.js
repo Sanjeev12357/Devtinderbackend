5f2304f01a0929a0c12a1d880b4e6d655f600437
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2025-05-21 17:41:25 +0530

    checkpoint 2

--- src/app.js
+++ src/app.js
@@ -1,36 +1,35 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const app = express();
+
+// Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", // replace with your frontend domain on production
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
 
+// Routers
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
-
 const userRouter = require("./routes/user");
 const requestRouter = require("./routes/request");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
-app.use('/',requestRouter);
+app.use("/", requestRouter);
 app.use("/", userRouter);
 
+// Connect DB
 connectDB()
-  .then(() => {
-    console.log("Database connection established...");
-    app.listen(7777, () => {
-      console.log("Server is successfully listening on port 7777...");
-    });
-  })
-  .catch((err) => {
-    console.error("Database cannot be connected!!");
-  });
+  .then(() => console.log("Database connection established..."))
+  .catch((err) => console.error("Database cannot be connected!!", err));
+
+// ❗️Don't use app.listen
+module.exports = app;
